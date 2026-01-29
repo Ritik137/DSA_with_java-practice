5188968c5f884612f2b2a557cee39d3d775ba442
Ritik Anand <anshritik1@gmail.com>
2026-01-30 02:03:05 +0530

    Add 3Sum solution using Two Pointers

---
+++ Arrayss/Two-Pointer/java_06_ThreeSum.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class java_06_ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); // step 1: sort array
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // skip duplicate for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    // skip duplicates for left & right
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // ---------- main method ----------
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> result = threeSum(nums);
+
+        System.out.println("Triplets with sum 0:");
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
