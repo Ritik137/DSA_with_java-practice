409f7cf1bab12b681b4b2d93d478174231af0d70
Ritik Anand <anshritik1@gmail.com>
2026-05-08 01:31:09 +0530

    update movezero code

--- Arrayss/Two-Pointer/Java_01_MoveZero.java
+++ Arrayss/Two-Pointer/Java_01_MoveZero.java
@@ -2,21 +2,19 @@ public class Java_01_MoveZero {
     public static void main(String[] args) {
         int arr[] = {1, 0, 6, 8, 0, 4, 0, 9};
 
-        int pos = 0; // position for next non-zero
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                if (i != pos) {   // only swap when needed
-                    int temp = arr[i];
-                    arr[i] = arr[pos];
-                    arr[pos] = temp;
-                }
-                pos++;
+        
+        int left =0;
+        int n=  arr.length;
+        for (int right=0; right<n; right++){
+            if(arr[right]!=0){
+                int temp = arr[right];
+                arr[right] = arr[left];
+                arr[left] = temp;
+                left++;
             }
         }
-
-        // print result
-        for (int x : arr) {
-            System.out.print(x + " ");
+        for (int i : arr) {
+            System.out.print(i+" ");
         }
     }
 }
